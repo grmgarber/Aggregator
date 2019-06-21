89330e446d463221e754304cb107474667841a44
gmgarber-sixm <Gregory.Garber@siriusxm.com>
2019-06-20 22:12:14 -0400

    really asynchronous!

--- app/controllers/aggregates_controller.rb
+++ app/controllers/aggregates_controller.rb
@@ -11,17 +11,15 @@ class AggregatesController < ApplicationController
   def index
     clients = {}
     responses = {}
-    tasks = []
-    SERVICES.each do |service|
-      url = "#{ROOT_SERVICE_URL}/#{service}"
-
-      Async do |task|
-        tasks << task
-        clients[service] = Async::HTTP::Internet.new
-        task.with_timeout(10) do
-          responses[service] = clients[service].get(url).read
-        rescue Async::TimeoutError
-          responses[service] = 'TIMED OUT'
+    urls = {}
+    Async do
+      SERVICES.each do |service|
+        Async.run do
+          urls[service] = "#{ROOT_SERVICE_URL}/#{service}"
+          clients[service] = Async::HTTP::Internet.new
+          puts "#{service} started at #{Time.now}"
+          responses[service] = clients[service].get(urls[service]).read
+          puts "#{service} finished at #{Time.now}"
         rescue => exc
           responses[service] = "Exception: #{exc}"
         ensure
@@ -29,8 +27,6 @@ class AggregatesController < ApplicationController
         end
       end
     end
-
-    results = tasks.map(&:wait)
 
     render json: responses
   end
